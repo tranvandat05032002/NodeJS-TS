88e609f6a928ca4279e4def7348dd507ab8ff0c3
tranvandat05032002 <tranvandathtml0503@gmail.com>
2023-06-30 22:51:42 +0700

    constructure project nodeJS

---
+++ configProjectNodeJS/src/index.ts
@@ -0,0 +1,9 @@
+const sum = (a: number, b: number) => {
+  return a + b
+}
+type Handle = () => Promise<string>
+const fullName = 'Tran Van Dat'
+const handleFullName: Handle = () => {
+  return Promise.resolve(fullName)
+}
+handleFullName().then(console.log)
