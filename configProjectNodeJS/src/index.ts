cc69d24e64ab64d5701b3ab4afeebe079d26cd2a
tranvandat05032002 <tranvandathtml0503@gmail.com>
2023-06-30 22:52:34 +0700

    fix bug type error in the typescript
    update

--- configProjectNodeJS/src/index.ts
+++ configProjectNodeJS/src/index.ts
@@ -7,3 +7,20 @@ const handleFullName: Handle = () => {
   return Promise.resolve(fullName)
 }
 handleFullName().then(console.log)
+
+const address: string = 'Tu Xuong'
+
+console.log(address)
+
+interface User {
+  name: string
+  age?: number
+}
+
+const render = (user: User) => {
+  console.log(user)
+}
+const user = { name: 'TranVanDat' }
+render(user as User)
+// render(user as any)
+render(user)
